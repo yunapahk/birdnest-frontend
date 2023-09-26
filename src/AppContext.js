9885d3948899ee180cdd7ed1122b41b8cbcbcc28
Yuna Pahk <new1@new1s-MacBook-Pro.local>
2023-09-26 15:31:37 -0700

    navbar filter working

---
+++ src/AppContext.js
@@ -0,0 +1,48 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+
+const AppContext = createContext();
+
+export function AppProvider({ children }) {
+  const [loading, setLoading] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState('All');
+  const [allPosts, setAllPosts] = useState([]);
+  const [filteredPosts, setFilteredPosts] = useState([]);
+
+  const fetchPosts = async () => {
+    setLoading(true);
+    const url = 'https://birdnest-backend-bc7d.onrender.com/birdnests/';
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setAllPosts(data);
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  
+
+  // Initial fetch
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  useEffect(() => {
+    setFilteredPosts(
+      selectedCategory === 'All'
+        ? allPosts
+        : allPosts.filter((post) => post.category === selectedCategory)
+    );
+  }, [selectedCategory, allPosts]);
+
+  return (
+    <AppContext.Provider value={{ loading, selectedCategory, setSelectedCategory, filteredPosts, fetchPosts }}>
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+export function useAppContext() {
+  return useContext(AppContext);
+}
